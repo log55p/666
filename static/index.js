9d0ca8500bcb190eed0aa4168dec6e4676a5731e
log55p <98441141+log55p@users.noreply.github.com>
2022-01-27 15:48:47 +0800

    Create index.js

--- static/index.js
+++ static/index.js
@@ -477,11 +477,11 @@ function shareText(score) {
     if (deviation_time > __Time * 1000 + 1000) {
         return '实际时间比设置时间多了' + ((deviation_time / 1000) - __Time).toFixed(2) + "秒，本次成绩作废哦！";
     }
-    if (score <= 2.5 * __Time) return '加油！我相信您可以的！';
-    if (score <= 5 * __Time) return '^_^ 加把劲，底力大王就是您！';
-    if (score <= 7.5 * __Time) return '您！';
-    if (score <= 10 * __Time) return '太 您 了！';
-    return '您是外星人嘛？';
+    if (score <= 2.5 * __Time) return '学通为你扣L';
+    if (score <= 5 * __Time) return '学通表示就这';
+    if (score <= 7.5 * __Time) return '学通给您拜年';
+    if (score <= 10 * __Time) return '学通给您磕头';
+    return '学通给你草';
 }
 
 function toStr(obj) {
